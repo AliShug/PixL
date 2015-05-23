503639c41984658f5d846615d37eada51c13658a
Alistair <alistair.wk@gmail.com>
2015-05-23 18:15:13 +0100

    Things

---
+++ newserver.js
@@ -0,0 +1,14 @@
+// Load the web-server, file-system and file-path modules.
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+
+// Load the python shell
+var pysh = require('python-shell');
+
+// The database 
+var db = require('mongojs').connect("mydb", ["test"]);
+
+var ports = [80, 443];
+
