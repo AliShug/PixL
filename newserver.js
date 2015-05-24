dfc3f5a8b767cb31d86e88eae6189555dade5540
Alistair <alistair.wk@gmail.com>
2015-05-24 13:29:39 -0400

    Stuff

--- newserver.js
+++ newserver.js
@@ -15,28 +15,48 @@ var ports = [80, 443];
 var express = require('express');
 var app = express();
 
+// Config
 app.set('views', './template');
+app.set('case sensitive routing', true);
+
+// Templating with jade
+app.set('view engine', 'jade');
+
+// Serving requests
+app.get('/', function(req, res) {
+    console.log(req.url);
+    res.render('test', {title: 'Hey', message: 'Hello there!'});
+});
+
+// (html files are *almost* all dynamically generated)
+app.get('/:file.html', function(req, res, next) {
+    // Google verification
+    if (req.url.indexOf('google') > -1) {
+        next();
+        return;
+    }
+
+    console.log('Serving ' + req.url);
+    console.log('        ' + req.path);
+    console.log('        ' + req.params.file);
+    res.render('test', {title: 'Hey', message: 'Hello there!'});
+});
+
+app.get('/test', function(req, res) {
+    res.send('Hello world!');
+});
+
 
 // Static file serving (from public dir)
 app.use(express.static('public'));
 
-// Templating through jade
-app.set('view engine', 'jade');
-
-app.get('/', function(req, res) {
-    res.send('Hello world!');
-});
-
-app.get('/test', function(req, res) {
-    res.render('test', {title: 'Hey', message: 'Hello there!'});
-});
-
-var server = app.listen(80, function() {
+// Start the server
+var server = app.listen(ports[0], function() {
 
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Webserver listening at http://%s:%s', host, port);
 
 });
 
