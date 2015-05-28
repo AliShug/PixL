9e15c577d16835fc3e5261cb08015ab31d86fcfb
Alistair <alistair.wk@gmail.com>
2015-05-28 10:51:43 -0400

    Wroking signup and simple user page

--- newserver.js
+++ newserver.js
@@ -26,6 +26,7 @@ var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var validator = require('express-validator');
 
 // Config
 var ports = [80, 443];
@@ -51,10 +52,50 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(multer());
+app.use(validator());
+
+// Custom user middleware
+app.use(function (req, res, next) {
+    if (req.session.loggedIn) {
+        db.users.findOne({_id:mongojs.ObjectId(req.session.user_id)}, function(err, user) {
+            if (user) {
+                req.user = user;
+                req.user.loggedIn = true;
+            }
+            else {
+                req.session.loggedIn = false;
+                req.user = {loggedIn:false};
+            }
+            next();
+        });
+    }
+    else {
+        req.user = {loggedIn:false};
+        next();
+    }
+});
+
+
+// User pages
+app.get('/user/:username', function(req, res, next) {
+    db.users.findOne({username:req.params.username}, function(err, user) {
+        if (user) {
+            res.render('user', {
+                title:'User '+user.username,
+                user:req.user,
+                renderUser:user
+            });
+        }
+        else {
+            // Fallthrough to 404
+            next();
+        }
+    });
+});
 
 // Signup
 app.get('/signup', function(req, res) {
-    if (req.session.loggedIn) {
+    if (req.user.loggedIn) {
         res.redirect(301, '/');
         return;
     }
@@ -62,19 +103,66 @@ app.get('/signup', function(req, res) {
     res.render('signup', {title:'PixL - Signup', file:'signup'});
 });
 app.post('/signup', function(req, res) {
-    if (req.session.loggedIn) {
+    if (req.user.loggedIn) {
         res.redirect(301, '/');
         return;
     }
 
-    console.log(req.body);
-    req.session.loggedIn = true;
-    res.render('signup', {title:'Created account!', login_success:true, file:'signup'});
+    // Check input
+    req.checkBody('name', 'Invalid name').notEmpty();
+    req.checkBody('username', 'username of length > 3 required').len(3,100);
+    req.checkBody('email', 'valid email required').isEmail();
+    req.checkBody('password', 'password of length 6-20 characters required').len(6,20);
+    
+    if (req.validationErrors()) {
+        res.render('signup', {
+            title:'PixL - Signup',
+            file:'signup',
+            errors:req.validationErrors(true),
+            inputs:{
+                name:req.body.name,
+                username:req.body.username,
+                email:req.body.email
+            }
+        });
+    }
+    else {
+        // Check against stored usernames
+        db.users.findOne({username:req.body.username}, function(err, user) {
+            if (!user) {
+                // Create the account
+                db.users.insert({
+                    name:req.body.name,
+                    username:req.body.username,
+                    email:req.body.email,
+                    password:req.body.password
+                });
+                res.render('signup', {title:'Created account!', login_success:true, file:'signup'});
+            }
+            else {
+                // "Already exists" error
+                res.render('signup', {
+                    title:'PixL - Signup',
+                    file:'signup',
+                    errors:{
+                        username:{
+                            msg:'username already exists'
+                        }
+                    },
+                    inputs:{
+                        name:req.body.name,
+                        username:req.body.username,
+                        email:req.body.email
+                    }
+                });
+            }
+        });
+    }
 });
 
 // Login
 app.get('/login', function(req, res) {
-    if (req.session.loggedIn) {
+    if (req.user.loggedIn) {
         res.redirect(301, '/');
         return;
     }
@@ -82,14 +170,51 @@ app.get('/login', function(req, res) {
     res.render('login', {title:'PixL - Login', file:'login'});
 });
 app.post('/login', function(req, res) {
-    if (req.session.loggedIn) {
+    if (req.user.loggedIn) {
         res.redirect(301, '/');
         return;
     }
 
-    console.log(req.body);
-    req.session.loggedIn = true;
-    res.render('login', {title:'Logged in!', login_success:true, file:'login'});
+    // Check input
+    req.checkBody('email').isEmail();
+    req.checkBody('password').notEmpty();
+
+    if (req.validationErrors()) {
+        res.render('login', {
+            title:'PixL - Login',
+            file:'login',
+            inputs:{
+                email:req.body.email
+            }
+        });
+    }
+    else {
+        db.users.findOne({
+            email:req.body.email,
+            password:req.body.password
+        }, function(err, user) {
+            if (user) {
+                req.session.user_id = user._id;
+                console.log('Logged in ' + user.username);
+                req.session.loggedIn = true;
+                res.render('login', {
+                    title:'Logged in!',
+                    login_success:true,
+                    file:'login',
+                    user:req.user
+                });
+            }
+            else {
+                res.render('login', {
+                    title:'PixL - Login',
+                    file:'login',
+                    inputs:{
+                        email:req.body.email
+                    }
+                });
+            }
+        });
+    }
 });
 
 // Logout (just destroy the session)
@@ -102,20 +227,9 @@ app.all('/logout', function(req,res) {
 app.get('/', function(req, res) {
     db.pages.findOne({file: 'landing'}, function(err, pageInfo) {
         db.items.findOne({_id:mongojs.ObjectId(pageInfo.featured_id)}, function(err, item) {
-            function land(err, user) {
-                if (!user) user = {};
-                if (!user.name) user.loggedIn = false;
-                pageInfo.user = user;
-                pageInfo.featured = item;
-                res.render(pageInfo.file, pageInfo);
-            }
-
-            if (req.session.loggedIn) {
-                db.users.findOne({_id:req.session.user_id}, land);
-            }
-            else {
-                land(null, {loggedIn:false});
-            }
+            pageInfo.user = req.user;
+            pageInfo.featured = item;
+            res.render(pageInfo.file, pageInfo);
         });
     });
 });
@@ -123,12 +237,10 @@ app.get('/', function(req, res) {
 // Channels
 app.get('/channels', function(req, res) {
     db.pages.findOne({file:'channels'}, function(err, pageInfo) {
-        db.users.findOne({}, function(err, user) {
-            db.channels.find({}, function(err, channels) {
-                pageInfo.channels = channels;
-                pageInfo.user = user;
-                res.render(pageInfo.file, pageInfo);
-            });
+        db.channels.find({}, function(err, channels) {
+            pageInfo.channels = channels;
+            pageInfo.user = req.user;
+            res.render(pageInfo.file, pageInfo);
         });
     });
 });
@@ -174,24 +286,22 @@ app.get('/search', function(req, res, next) {
             break;
     }
 
-    db.users.findOne({}, function(err,user) {
-        db.items.find(  {$text : {$search:query.q}},
-                        {score:{$meta:"textScore"}}).sort(sorter,
-                        function(err, items) {
+    db.items.find(  {$text : {$search:query.q}},
+                    {score:{$meta:"textScore"}}).sort(sorter,
+                    function(err, items) {
 
-            var params = {
-                url : req.url,
-                title : 'Search',
-                results : items,
-                user : user,
-                query : query.q,
-                sort : sort,
-                ord : ord
-            };
-            
-            console.log(params);
-            res.render('search', params);
-        });
+        var params = {
+            url : req.url,
+            title : 'Search',
+            results : items,
+            user : req.user,
+            query : query.q,
+            sort : sort,
+            ord : ord
+        };
+        
+        console.log(params);
+        res.render('search', params);
     });
 });
 
@@ -203,32 +313,30 @@ app.get('/item/:itemid', function(req, res, next) {
         return;
     }
 
-    db.users.findOne({}, function(err, user) {
-        console.log(req.params.itemid);
-        console.log(mongojs.ObjectId(req.params.itemid));
-        db.items.findOne({_id : mongojs.ObjectId(req.params.itemid.toString())}, function(err, item) {
-            if (!item) {
-                console.log("Item " + req.params.itemid + ": unrecognized ID");
-                next();
-                return;
-            }
+    //console.log(req.params.itemid);
+    //console.log(mongojs.ObjectId(req.params.itemid));
+    db.items.findOne({_id : mongojs.ObjectId(req.params.itemid.toString())}, function(err, item) {
+        if (!item) {
+            console.log("Item " + req.params.itemid + ": unrecognized ID");
+            next();
+            return;
+        }
 
-            var params = {
-                title : item.name,
-                item : item,
-                user : user
-            }
+        var params = {
+            title : item.name,
+            item : item,
+            user : req.user
+        }
 
-            // Register a view on the item
-            db.items.update({_id : item._id}, {$inc:{views:1}});
+        // Register a view on the item
+        db.items.update({_id : item._id}, {$inc:{views:1}});
 
-            if (item.type === '3d') {
-                res.render('item_3d', params);
-            }
-            else if (item.type === 'img') {
-                res.render('item_img', params);
-            }
-        });
+        if (item.type === '3d') {
+            res.render('item_3d', params);
+        }
+        else if (item.type === 'img') {
+            res.render('item_img', params);
+        }
     });
 });
 
@@ -258,7 +366,7 @@ app.use(function(req, res, next) {
 
     // respond with html page
     if (req.accepts('html')) {
-        res.render('404', { url: req.url });
+        res.render('404', { title:'Not Found', url: req.url, user:req.user });
         return;
     }
 
